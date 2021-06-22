89f966d2b7e7042bdc12ed569138fa8dd54fe20b
Ian Lambert <lambe347@umn.edu>
2021-06-21 21:40:35 -0500

    route for getting a random quote added

---
+++ server/routes/random.quote.router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+//need to add the random nature to this part, currently just grabs the first quote
+router.get('/', (req, res) => {
+    const queryText = `SELECT * FROM "quotes" WHERE quotes.id = $1`
+    pool.query(queryText, [1])
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('Error with getting random quote', error);
+            res.sendStatus(500);
+        })
+})
+
+module.exports = router;
