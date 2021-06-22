5a0df5fa1a3d6263056f51e9ec3fe47591e14a70
Ian Lambert <lambe347@umn.edu>
2021-06-21 21:24:28 -0500

    random quote saga added

---
+++ src/redux/sagas/randomQuote.saga.js
@@ -0,0 +1,18 @@
+import { put, takeLatest } from "@redux-saga/core/effects";
+import axios from "axios";
+
+function* fetchRandomQuote(){
+    try {
+        const response = yield axios.get('/api/random-quote');
+        console.log(response.data);
+        yield put ({type: 'SET_RANDOM_QUOTE', payload: response.data});
+    } catch {
+        console.log('Error with getting random quote');
+    }
+}
+
+function randomQuoteSaga(){
+    yield takeLatest('FETCH_RANDOM_QUOTE', fetchRandomQuote);
+}
+
+export default randomQuoteSaga;
