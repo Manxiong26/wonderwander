4914ab5075da4d553717469f3a2e8172073326cd
Johnny Keple <Johnny.Keple@gmail.com>
2021-06-23 15:47:18 -0500

    Adding in details of art

--- src/components/SeePage/SeePage.jsx
+++ src/components/SeePage/SeePage.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { CardContent, Card, IconButton, Typography, CardMedia, makeStyles, } from "@material-ui/core";
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 function SeePage() {
+    // const list = useSelector((store) => store.artworkDetailReducer);
+    // const dispatch = useDispatch();
+
+    // useEffect(() => {
+    //     console.log('In useEffect param:');
+    //     dispatch({type: 'FETCH_ART_DETAIL', payload: id})
+    // }, []);
+
+    // const {id} = useParams();
+
+
+
     return (
 
         <section>
+            {/* <Card>
+                <CardMedia
+                component="video" 
+                image={list.artwork_vidlink}
+                >
+                </CardMedia>
+            </Card> */}
             <img src="https://lh6.ggpht.com/HlgucZ0ylJAfZgusynnUwxNIgIp5htNhShF559x3dRXiuy_UdP3UQVLYW6c=s1200">
             </img>
             <h3>
@@ -13,4 +35,6 @@ function SeePage() {
     );
 }
 
+
+// artwork_vidlink
 export default SeePage;
