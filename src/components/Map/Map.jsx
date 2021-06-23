b4e22ee1688c5b5ee2c00c9e8b310ebcab65e71c
Raveryk <raveryk@gmail.com>
2021-06-23 15:07:58 -0500

    Added geo location to Map component.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,25 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import env from "react-dotenv";
 import { makeStyles } from "@material-ui/core";
 import GoogleMapReact from "google-map-react";
 import "./Map.css";
 
 const useStyles = makeStyles((theme) => ({
-    map: {
-        marginRight: 'auto',
-        marginLeft: 'auto',
-        border: '1px solid black',
-        }
+  map: {
+    marginRight: "auto",
+    marginLeft: "auto",
+    border: "1px solid black",
+  },
 }));
 
 const Map = ({ mapLat, mapLng, zoom, reducer, height, width }) => {
+    // --- Geo Location --- //
+  let geo = navigator.geolocation;
+
+  geo.getCurrentPosition((position) => {
+    setUserLat(position.coords.latitude);
+    setUserLng(position.coords.longitude);
+  });
+  // Load if location is available 
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      console.log("Available");
+      setGeoAvailable(true);
+    } else {
+      console.log("Not Available");
+      setGeoAvailable(false);
+    }
+  }, []);
+
+  const [userLat, setUserLat] = useState(0);
+  console.log("user lat: ", userLat);
+  const [userLng, setUserLng] = useState(0);
+  console.log("user lng: ", userLng);
+  const [geoAvailable, setGeoAvailable] = useState(false);
+
   const classes = useStyles();
 
-  // --IDEA: May need to map this component with the values
-  // of the artwork locations once server route and reducer is set up.
+  // Balloon marker
   const BalloonMarker = () => (
     <div className="mapMarker" onClick={toArtDetail}></div>
   );
+  // User location marker
+  const UserLocation = () => <div className="userMarker"></div>;
 
   // Props values for map
   // center and zoom
@@ -37,17 +62,20 @@ const Map = ({ mapLat, mapLng, zoom, reducer, height, width }) => {
   };
 
   return (
-    <div style={{height: height, width: width}} className={classes.map}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_KEY }}
-        defaultCenter={locationVars.center}
-        defaultZoom={locationVars.zoom}
-      >
-        {reducer.map((item, i) => (
-        <BalloonMarker key={i} lat={item.lat} lng={item.long}/>
-    ))}
-      </GoogleMapReact>
-    </div>
+        <div style={{ height: height, width: width }} className={classes.map}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_KEY }}
+            defaultCenter={locationVars.center}
+            defaultZoom={locationVars.zoom}
+          >
+            {reducer.map((item, i) => (
+              <BalloonMarker key={i} lat={item.lat} lng={item.long} />
+            ))}
+            {geoAvailable ? (
+            <UserLocation lat={userLat} lng={userLng} />
+            ) : ('')}
+          </GoogleMapReact>
+        </div>
   );
 };
 
