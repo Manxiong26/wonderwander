d455ffdfa3b87d7fce1d05c267b7b5d39b5d6ae4
Raveryk <raveryk@gmail.com>
2021-06-24 23:25:09 -0500

    Connected artist detail to artwork detail and started styling page.

--- server/routes/artist.router.js
+++ server/routes/artist.router.js
@@ -8,7 +8,7 @@ router.get('/:id', (req, res) => {
     const artistId = req.params.id
     console.log('Artist ID: ', artistId)
 
-    const query = `SELECT artist.name, artist.image, artist.bio, artist.site_link, artwork.name as title, artwork.image as art_pic FROM artwork 
+    const query = `SELECT artist.name, artist.image, artist.bio, artist.site_link, artwork.id as art_id, artwork.name as title, artwork.image as art_pic FROM artwork 
     JOIN artist ON artwork.artist_id=artist.id
     WHERE artist.id=$1 AND artist.published=true AND artwork.published=true;`;
 
