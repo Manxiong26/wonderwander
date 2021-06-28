8ab3507106bca863a2c415c600ecf744db333276
Shyla Earl (she/her) <shyla.earl@gmail.com>
2021-06-28 18:05:05 -0500

    see/do inputs added to artAdventure, POST routes functional, See get route for specific adventure functional

--- src/components/AdminArtAdventure/AdminArtAdventure.jsx
+++ src/components/AdminArtAdventure/AdminArtAdventure.jsx
@@ -29,10 +29,16 @@ function AdminArtAdventure() {
     const artAdventureList = useSelector((store) => store.adminArtAdventureListReducer);
     const artAdventure = useSelector((store) => store.adminArtAdventureInfoReducer);
     console.log('artAdventure reducer id:', artAdventure.id);
+    const seeList = useSelector((store) => store.adminSeeListReducer);
+    const see = useSelector ((store) => store.adminSeeInfoReducer);
+    //const doList = useSelector((store) => store.adminDoListReducer);
+    //const doItem = useSelector((store) => store.adminDoReducer);
 
     //retrieves art adventures' info from DB
     useEffect(() => {
         dispatch({ type: 'FETCH_ADVENTURE_LIST' });
+        //dispatch({ type: 'FETCH_SEE_LIST' });
+        //dispatch({ type: 'FETCH_DO_LIST' });
     }, []);
 
     //sets local state for post request
@@ -76,7 +82,8 @@ function AdminArtAdventure() {
         console.log('clicking edit for Art Adventure = ', item);
 
         //sets specific art adventure in art adventure reducer 
-        dispatch({type: 'SET_ADVENTURE_INFO', payload: item}); // 
+        dispatch({type: 'SET_ADVENTURE_INFO', payload: item});
+        dispatch({type: 'FETCH_SEE_LIST', payload: item.id}); 
 
         //renders form view from add to edit mode
         setEditMode(true);
@@ -156,6 +163,73 @@ function AdminArtAdventure() {
           }
         });
     }
+
+    //sets local state for See/Do post request
+    const [do_prompts, setDoPrompts] = useState('');
+    const [see_prompts, setSeePrompts] = useState('');
+    const [link, setLink] = useState('');
+    const [adventure_id, setAdventureId] = useState('');
+    const [seeId, setSeeId] = useState('');
+
+    const addSee = () => {
+        console.log('Add See Clicked.');
+
+        //create object to send
+        const newSee = {
+            prompts: see_prompts,
+            link: link,
+            artwork_id: null,
+            activity_id: adventure_id,
+        }
+        console.log('Adding see object: ', newSee);
+
+        //dispatch to artAdventure saga
+        dispatch({ type: 'ADD_SEE', payload: newSee });
+
+        //updates see list on DOM (where/are we listing the see prompts ?)
+
+        //alert successful post
+        swal({
+            text: `This 'See' has been added to your activity!`,
+            icon: "success"
+        });
+
+        //TODO - Reset dropdown to default value
+
+        //clears input fields
+        setSeePrompts('');
+        setLink('');
+        setAdventureId('');
+    }
+
+    const addDo = () => {
+        console.log('Add Do Clicked.');
+
+        //create object to send
+        const newDo = {
+            prompts: do_prompts,
+            artwork_id: null,
+            activity_id: adventure_id,
+        }
+        console.log('Adding do object: ', newDo);
+
+        //dispatch to artAdventure saga
+        dispatch({ type: 'ADD_DO', payload: newDo });
+
+        //updates do list on DOM (where/are we listing the do prompts ?)
+
+        //alert successful post
+        swal({
+            text: `This 'Do' has been added to your activity!`,
+            icon: "success"
+        });
+
+        //TODO - Reset dropdown to default value
+
+        //clears input fields
+        setDoPrompts('');
+        setAdventureId('');
+    }
   
     return (
       <div>
@@ -179,6 +253,24 @@ function AdminArtAdventure() {
                     value={image}
                     onChange={(event) => setImage(event.target.value)}
                 />
+                {/* generates 'See' options dynamically */}
+                <select type="text"
+                    onChange={(event) => setSeeId(event.target.value)}
+                    >
+                    <option value="Default">See</option>
+                    {seeList.map((see) => {
+                        return (<option key={see.id} value={see.id}>{see.prompts}</option>);
+                    })}
+                </select>
+                {/* generates 'Do' options dynamically */}
+                {/* <select type="text"
+                    onChange={(event) => setDoId(event.target.value)}
+                    >
+                    <option value="Default">Do</option>
+                    {doList.map((doItem) => {
+                        return (<option key={doItem.id} value={doItem.id}>{doItem.prompts}</option>);
+                    })}
+                </select> */}
                 <Button className="admin-btn" type="submit" name="submit" variant="outlined" value="Update">Update</Button>
                 <Button className="admin-btn" variant="outlined" onClick={renderToInfo}>Cancel</Button>
               </form>
@@ -202,6 +294,51 @@ function AdminArtAdventure() {
                     value={image}
                     onChange={(event) => setImage(event.target.value)}
                 />
+                <Button className="admin-btn" type="submit" name="submit" variant="outlined" value="Submit">Submit</Button>
+              </form>
+
+              {/* Add See Form */}
+              <Typography variant="h4">Add See</Typography>
+              <form className="admin-form" onSubmit={addSee}>
+                <TextField type="text"
+                    placeholder="Prompt"
+                    value={see_prompts}
+                    onChange={(event) => setSeePrompts(event.target.value)}
+                />
+                <TextField type="text"
+                    placeholder="Image/Video URL"
+                    value={link}
+                    onChange={(event) => setLink(event.target.value)}
+                />
+                {/* generates adventure options dynamically */}
+                <select type="text"
+                    onChange={(event) => setAdventureId(event.target.value)}
+                    >
+                    <option value="Default">Adventure</option>
+                    {artAdventureList.map((adventure) => {
+                        return (<option key={adventure.id} value={adventure.id}>{adventure.title}</option>);
+                    })}
+                </select>
+                <Button className="admin-btn" type="submit" name="submit" variant="outlined" value="Submit">Submit</Button>
+              </form>
+
+              {/* Add Do Form */}
+              <Typography variant="h4">Add Do</Typography>
+              <form className="admin-form" onSubmit={addDo}>
+                <TextField type="text"
+                    placeholder="Prompt"
+                    value={do_prompts}
+                    onChange={(event) => setDoPrompts(event.target.value)}
+                />
+                {/* generates adventure options dynamically */}
+                <select type="text"
+                    onChange={(event) => setAdventureId(event.target.value)}
+                    >
+                    <option value="Default">Adventure</option>
+                    {artAdventureList.map((adventure) => {
+                        return (<option key={adventure.id} value={adventure.id}>{adventure.title}</option>);
+                    })}
+                </select>
                 <Button className="admin-btn" type="submit" name="submit" variant="outlined" value="Submit">Submit</Button>
               </form>
           </div>}
--- src/redux/sagas/adminArtAdventure.saga.js
+++ src/redux/sagas/adminArtAdventure.saga.js
@@ -54,12 +54,57 @@ function* deleteArtAdventure(action) {
     }
 }
 
+function* getSeeList(action) {
+  try {
+    const sees = yield axios.get(`/api/admin/art-adventure/${action.payload}/see`); 
+    console.log('get all sees for adventure:', sees.data);
+    yield put({ type: 'SET_SEE', payload: sees.data });
+  } catch (error) {
+    console.log('Art adventure SEE List get request failed', error);
+  }
+}
+
+function* getSee(action) {
+  try {
+    const see = yield axios.get(`/api/admin/art-adventure/see/${action.payload}`); 
+    console.log('get one specific SEE for adventure:', see.data[0]);
+    yield put({ type: 'SET_SEE_INFO', payload: see.data[0] });
+    
+  } catch (error) {
+    console.log('Art adventure SEE get request failed: ', error);
+  }
+}
+
+function* addSee(action) {
+  try{
+      console.log('New See for Adventure:', action.payload);
+      yield axios.post('/api/admin/art-adventure/see', action.payload);
+      yield put({type: 'FETCH_SEE_LIST' }); //Double check on this action, etc.
+  } catch (error) {
+      console.log(`Error adding 'See' to adventure: `, error);
+  }
+}
+
+function* addDo(action) {
+  try{
+      console.log('New Do for Adventure:', action.payload);
+      yield axios.post('/api/admin/art-adventure/do', action.payload);
+      //yield put({ }) TODO - Does this need to be added to a list reducer for any reason?
+  } catch (error) {
+      console.log(`Error adding 'Do' to adventure: `, error);
+  }
+}
+
 function* adminArtAdventureSaga() {
   yield takeEvery('FETCH_ADVENTURE_LIST', getArtAdventureList);
   yield takeEvery('FETCH_ADVENTURE', getArtAdventure);
   yield takeEvery('ADD_ADVENTURE', addArtAdventure);
   yield takeEvery('UPDATE_ADVENTURE_INFO', updateArtAdventure); 
   yield takeEvery('DELETE_ADVENTURE', deleteArtAdventure);
+  yield takeEvery('FETCH_SEE_LIST', getSeeList);
+  yield takeEvery('FETCH_SEE', getSee);
+  yield takeEvery('ADD_SEE', addSee);
+  yield takeEvery('ADD_DO', addDo);
 }
 
 export default adminArtAdventureSaga;
