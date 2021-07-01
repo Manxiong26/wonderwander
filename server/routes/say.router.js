4cac97c1833699b74d4d800277537de957886208
Johnny Keple <Johnny.Keple@gmail.com>
2021-07-01 15:47:18 -0500

    Getting numbers for votes to show up

--- server/routes/say.router.js
+++ server/routes/say.router.js
@@ -34,19 +34,19 @@ router.post('/', (req, res) => {
 });
 
 
-// router.get('/:id', (req, res) => {
-//     console.log('Start of the see page', req.params);
-//     const queryText = `
-//     SELECT say_id, COUNT (*) FROM "say_poll" JOIN "say" ON "say".id = "say_poll".say_id WHERE "say_poll".artwork_id = $1 GROUP BY "say_poll".say_id;`;
-//     pool.query(queryText)
-//         .then(result => {
-//             console.log('END OF THE SEE PAGE', req.params);
-//             res.send(result.rows);
-//         })
-//         .catch(error => {
-//             console.log('Error with getting see', error);
-//             res.sendStatus(500);
-//         })
-// })
+router.get('/count/:id', (req, res) => {
+    console.log('Start of the see page vote', req.params);
+    const queryText = `
+    SELECT "say_poll".say_id, COUNT (*) FROM "say_poll" JOIN "say" ON "say".id = "say_poll".say_id WHERE "say_poll".artwork_id = $1 GROUP BY "say_poll".say_id ORDER BY "say_poll".say_id ASC;`;
+    pool.query(queryText, [req.params.id])
+        .then(result => {
+            console.log('END OF THE SEE PAGE vOTE', req.params);
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('Error with getting see', error);
+            res.sendStatus(500);
+        })
+});
 
 module.exports = router;
--- src/redux/reducers/addVote.reducer.js
+++ src/redux/reducers/addVote.reducer.js
@@ -1,10 +1,11 @@
-const addVote = (state = [], action) => {
-    switch (action.type) {
-      case 'SET_VOTE':
-        return action.payload;
-      default:
-        return state;
-    }
-  };
+// const addVote = (state = [], action) => {
+//     switch (action.type) {
+//       case 'FETCH_TOTAL_VOTE':
+//           //Write a if statement here
+//         return action.payload;
+//       default:
+//         return state;
+//     }
+//   };
   
-  export default addVote;
+//   export default addVote;
